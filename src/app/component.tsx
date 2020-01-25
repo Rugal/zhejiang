f57e6beeb4eef747c1b89a8aa56de2ae6fa7ebdf
Rugal Bernstein <rugal.bernstein@thomsonreuters.com>
2020-01-25 18:58:21 +0800

    Add zhejiang map

--- src/app/component.tsx
+++ src/app/component.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 
+import Body from "../component/body";
+
 const App: React.FC = () => {
-  return <React.Fragment />;
+  return (
+    <React.Fragment >
+      <Body />
+    </React.Fragment>
+  );
 };
 export default App;
---
+++ src/component/body/component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { ReactComponent as Map } from "./image/map.svg";
+
+const Body: React.FC = () => {
+  return (
+    <React.Fragment >
+      <Map />
+    </React.Fragment>
+  );
+};
+export default Body;
