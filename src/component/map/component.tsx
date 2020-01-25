3d27293908959fbba2e48c77eb0582f991d7a1a9
Rugal Bernstein <rugal.bernstein@thomsonreuters.com>
2020-01-26 02:36:20 +0800

    Use better color schema

--- src/component/map/component.tsx
+++ src/component/map/component.tsx
@@ -21,20 +21,33 @@ interface IProps {
   statistics: IStatistics[];
 }
 
+//
+// this function is very important
+//
+const getClassName = (s: IStatistics) => {
+  if (s.incident < 1)
+    return `${s.className} safe`;
+  if (s.incident < 3)
+    return `${s.className} info`;
+  if (s.incident < 8)
+    return `${s.className} warn`;
+  else
+    return `${s.className} error`;
+};
+
 const getPath = (cname: string, d: string) => <path className={cname} d={d} />;
 const getPaths = (cname: string, s: IStatistics) => s.path.map((p, i) => getPath(cname, p.d));
 const getPolygon = (cname: string, points: string) => <polygon className={cname} points={points} />;
-const getPolygons = (cname: string, s: IStatistics) => s.polygon &&
-  s.polygon.map((p, i) => getPolygon(cname, p.points));
+const getPolygons = (cname: string, s: IStatistics) => s.polygon && s.polygon.map((p) => getPolygon(cname, p.points));
 
 const Map: React.FC<IProps> = (p) => (
   <svg width="800px" height="800px">
     {p.statistics.map((s: IStatistics, i) =>
       <React.Fragment key={i}>
-        {getPaths(s.className, s)}
-        {getPolygons(s.className, s)}
+        {getPaths(getClassName(s), s)}
+        {getPolygons(getClassName(s), s)}
         <text key={i} transform={s.transform} >{s.name}: {s.incident}</text>
-      </React.Fragment>,
+      </React.Fragment>
     )}
   </svg>
 );
