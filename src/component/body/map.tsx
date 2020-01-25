7d6724d4a71ef896df12a6946fbb9a1c7adb6201
Rugal Bernstein <rugal.bernstein@thomsonreuters.com>
2020-01-26 01:44:05 +0800

    Add redux

--- src/component/body/map.tsx
+++ src/component/body/map.tsx
@@ -29,17 +29,6 @@ const getPolygons = (cname: string, s: IStatistics) => s.polygon && s.polygon.ma
 
 const Zhejiang: React.FC<IProps> = (p) =>
 	<svg width="800px" height="800px">
-		<path className="zhoushan" style={{fill: "none"}} d="M521.5,189.375c0.125,1.5-0.145,2.5,0.803,2.5s2.572-2.5,2.572-2.5l-1.688-1.235L521.5,189.375z" />
-	<path className="zhoushan" style={{fill: "none"}} d="M526.375,185.25c1.125,0.375,2.75,1.25,2.75,1.25s1-1.75,0-2s-2.375-1.125-2.75-0.75
-	S526.375,185.25,526.375,185.25z" />
-	<polygon className="zhoushan" style={{fill: "none"}} points="531.625,186.5 531.625,188.14 533.75,188.14 533.125,185.079 " />
-  <path className="zhoushan" style={{fill: "none"}} d="M567,180.625c0,0-3.25,4.563-2.125,5.563s3,2.063,3.25,0.313s1.5-2.813,1.5-4.688
-	S567,180.625,567,180.625z" />
-	<path className="zhoushan" style={{fill: "none"}} d="M535.633,161.75c0,0,2.117,3.125,3.117,2.875s5.063-5.063,5.063-5.063s-0.313-1.75-2.188-1.813
-	s-5.438,0.875-5.563,2.063S535.633,161.75,535.633,161.75z" />
-	<path className="zhoushan" style={{fill: "none"}} d="M521.5,162.063c0,0,4.375-1.438,3.938-2.375s-0.656-2.25-1.953-2.313s-5.172,0.688-4.109,1.875
-	s1.813,2.5,1.813,2.5L521.5,162.063z" />
-
 		{p.statistics.map((s: IStatistics, i) =>
 			<React.Fragment>
 				{getPaths(s.className, s)}
